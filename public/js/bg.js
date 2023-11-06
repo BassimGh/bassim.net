1318f1396a1b79cfb69be4e1e0162b6d91b9f35d
bassim <bassim.gh@icloud.com>
2023-11-05 19:16:04 -0800

    bug fixes

---
+++ public/js/bg.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#frontPage",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 100.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x8c8787,
+    points: 15.00,
+    maxDistance: 15.00,
+    spacing: 14.00,
+    showDots: false
+    });
